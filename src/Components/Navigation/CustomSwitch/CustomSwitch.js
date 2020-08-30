c9e8bfecce4fc11656377648d9fa19957671767b
victor.reis <VCREIS@brasilseg.com.br>
2020-08-30 19:36:47 -0300

    Center the content pages

--- src/Components/Navigation/CustomSwitch/CustomSwitch.js
+++ src/Components/Navigation/CustomSwitch/CustomSwitch.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 
 import HoursWorkedFlowchart from '../../../Pages/HoursWorkedFlowchart/HoursWorkedFlowchart';
 import createCustomRoute from './Components/CustomRoute';
+import classes from './CustomSwitch.module.css';
 
 const CustomSwitch = (props) => {
     const {items} = props;
@@ -10,10 +11,12 @@ const CustomSwitch = (props) => {
     const routes = items.map((item) => createCustomRoute(item)());
 
     return (
-        <Switch>
-            <Route path="/" exact component={HoursWorkedFlowchart} />
-            {routes}
-        </Switch>
+        <div className={classes.CustomSwitchContainer}>
+            <Switch>
+                <Route path="/" exact component={HoursWorkedFlowchart} />
+                {routes}
+            </Switch>
+        </div>
     );
 };
 
