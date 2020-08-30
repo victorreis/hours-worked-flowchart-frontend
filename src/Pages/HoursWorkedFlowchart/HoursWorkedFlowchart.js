a75a6b092a57d95f4ec7a1ba48f92485fd839f2a
victor.reis <VCREIS@brasilseg.com.br>
2020-08-30 19:44:55 -0300

    Add TimePickers and his icons to home page

--- src/Pages/HoursWorkedFlowchart/HoursWorkedFlowchart.js
+++ src/Pages/HoursWorkedFlowchart/HoursWorkedFlowchart.js
@@ -1,5 +1,69 @@
 import React from 'react';
+import {Icon, TextInput, Row, Col, TimePicker} from 'react-materialize';
 
-const HoursWorkedFlowchart = (props) => <div>HoursWorkedFlowchart!!!</div>;
+import {HOME_START_INPUT, HOME_FINISH_INPUT} from '../../Constants/Texts';
+import classes from './HoursWorkedFlowchart.module.css';
+
+const HoursWorkedFlowchart = (props) => (
+    <>
+        <Row>
+            <Col s={4}>
+                {HOME_START_INPUT}
+                <Row>
+                    <TimePicker
+                        icon={<Icon>schedule</Icon>}
+                        options={{
+                            autoClose: false,
+                            container: null,
+                            defaultTime: 'now',
+                            duration: 350,
+                            fromNow: 0,
+                            i18n: {
+                                cancel: 'Cancel',
+                                clear: 'Clear',
+                                done: 'Ok',
+                            },
+                            onCloseEnd: null,
+                            onCloseStart: null,
+                            onOpenEnd: null,
+                            onOpenStart: null,
+                            onSelect: null,
+                            showClearBtn: false,
+                            twelveHour: false,
+                            vibrate: true,
+                        }}
+                    />
+                </Row>
+                {HOME_FINISH_INPUT}
+                <Row>
+                    <TimePicker
+                        icon={<Icon>watch_later</Icon>}
+                        options={{
+                            autoClose: false,
+                            container: null,
+                            defaultTime: 'now',
+                            duration: 350,
+                            fromNow: 0,
+                            i18n: {
+                                cancel: 'Cancel',
+                                clear: 'Clear',
+                                done: 'Ok',
+                            },
+                            onCloseEnd: null,
+                            onCloseStart: null,
+                            onOpenEnd: null,
+                            onOpenStart: null,
+                            onSelect: null,
+                            showClearBtn: false,
+                            twelveHour: false,
+                            vibrate: true,
+                        }}
+                    />
+                </Row>
+            </Col>
+            <Col s={8}>Chart</Col>
+        </Row>
+    </>
+);
 
 export default HoursWorkedFlowchart;
