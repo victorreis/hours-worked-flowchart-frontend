35592d074f15a39e78f253111c6a02862fd25fc6
victor.reis <VCREIS@brasilseg.com.br>
2020-08-30 18:10:24 -0300

    Integrate react-router-dom with the existing pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 import withReduxFeatures from 'withReduxFeatures';
 
-import Example1 from './Pages/Example1/Example1';
+import About from './Pages/About';
+import Example1 from './Pages/Example1';
+import HoursWorkedFlowchart from './Pages/HoursWorkedFlowchart';
 
 const NonWrappedApp = () => (
     <>
-        <Example1 />
+        <Router>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/example">example</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route path="/example">
+                    <Example1 />
+                </Route>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/">
+                    <HoursWorkedFlowchart />
+                </Route>
+            </Switch>
+        </Router>
     </>
 );
 
