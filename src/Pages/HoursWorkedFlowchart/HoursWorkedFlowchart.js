7e09b0fe383efd2a7a72466151966420000e4c22
victor.reis <VCREIS@brasilseg.com.br>
2020-08-31 00:09:55 -0300

    Add TimePickers

--- src/Pages/HoursWorkedFlowchart/HoursWorkedFlowchart.js
+++ src/Pages/HoursWorkedFlowchart/HoursWorkedFlowchart.js
@@ -1,69 +1,73 @@
-import React from 'react';
-import {Icon, TextInput, Row, Col, TimePicker} from 'react-materialize';
+import React, {useRef} from 'react';
+import {Icon, Row, Col, TimePicker} from 'react-materialize';
+
+import moment from 'moment';
 
 import {HOME_START_INPUT, HOME_FINISH_INPUT} from '../../Constants/Texts';
 import classes from './HoursWorkedFlowchart.module.css';
 
-const HoursWorkedFlowchart = (props) => (
-    <>
-        <Row>
-            <Col s={4}>
-                {HOME_START_INPUT}
-                <Row>
-                    <TimePicker
-                        icon={<Icon>schedule</Icon>}
-                        options={{
-                            autoClose: false,
-                            container: null,
-                            defaultTime: 'now',
-                            duration: 350,
-                            fromNow: 0,
-                            i18n: {
-                                cancel: 'Cancel',
-                                clear: 'Clear',
-                                done: 'Ok',
-                            },
-                            onCloseEnd: null,
-                            onCloseStart: null,
-                            onOpenEnd: null,
-                            onOpenStart: null,
-                            onSelect: null,
-                            showClearBtn: false,
-                            twelveHour: false,
-                            vibrate: true,
-                        }}
-                    />
-                </Row>
-                {HOME_FINISH_INPUT}
-                <Row>
-                    <TimePicker
-                        icon={<Icon>watch_later</Icon>}
-                        options={{
-                            autoClose: false,
-                            container: null,
-                            defaultTime: 'now',
-                            duration: 350,
-                            fromNow: 0,
-                            i18n: {
-                                cancel: 'Cancel',
-                                clear: 'Clear',
-                                done: 'Ok',
-                            },
-                            onCloseEnd: null,
-                            onCloseStart: null,
-                            onOpenEnd: null,
-                            onOpenStart: null,
-                            onSelect: null,
-                            showClearBtn: false,
-                            twelveHour: false,
-                            vibrate: true,
-                        }}
-                    />
-                </Row>
-            </Col>
-            <Col s={8}>Chart</Col>
-        </Row>
-    </>
-);
+const timePickerOptions = {
+    autoClose: false,
+    container: null,
+    defaultTime: 'now',
+    duration: 0,
+    fromNow: 0,
+    i18n: {
+        cancel: 'Cancel',
+        clear: 'Clear',
+        done: 'Ok',
+    },
+    onCloseEnd: null,
+    onCloseStart: null,
+    onOpenEnd: null,
+    onOpenStart: null,
+    onSelect: null,
+    showClearBtn: false,
+    twelveHour: false,
+    vibrate: true,
+};
+
+const HoursWorkedFlowchart = (props) => {
+    const startTimePikerRef = useRef('');
+    const finishTimePikerRef = useRef('');
+
+    const handleChangeStartTimePicker = (hour, minute) => {
+        startTimePikerRef.current = moment(`${hour}:${minute}`);
+    };
+
+    const handleChangeFinishTimePicker = (hour, minute) => {
+        finishTimePikerRef.current = moment(`${hour}:${minute}`);
+    };
+
+    return (
+        <>
+            <Row>
+                <Col s={4}>
+                    {HOME_START_INPUT}
+                    <Row>
+                        <TimePicker
+                            ref={startTimePikerRef}
+                            icon={<Icon>schedule</Icon>}
+                            value={startTimePikerRef.current}
+                            onChange={handleChangeStartTimePicker}
+                            options={timePickerOptions}
+                        />
+                    </Row>
+                    {HOME_FINISH_INPUT}
+                    <Row>
+                        <TimePicker
+                            ref={finishTimePikerRef}
+                            icon={<Icon>schedule</Icon>}
+                            value={finishTimePikerRef.current}
+                            onChange={handleChangeFinishTimePicker}
+                            options={timePickerOptions}
+                        />
+                    </Row>
+                </Col>
+                <Col s={8}>Chart</Col>
+            </Row>
+        </>
+    );
+};
 
 export default HoursWorkedFlowchart;
