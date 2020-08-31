915803edda9b64d16d16984b532b078cbd67cf05
victor.reis <VCREIS@brasilseg.com.br>
2020-08-31 11:45:53 -0300

    Create CustomTimePicker

---
+++ src/Components/CustomTimePicker/CustomTimePicker.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {Icon, Row, Col, Range, TextInput, Card} from 'react-materialize';
+
+import classes from './CustomTimePicker.module.css';
+
+const CustomTimePicker = (props) => {
+    const {
+        title,
+        iconName,
+        hourInput,
+        handleChangeHourInput,
+        minuteInput,
+        handleChangeMinuteInput,
+    } = props;
+    return (
+        <Card title={title}>
+            <Row align="right">
+                <Col s={3} className={classes.iconPadding}>
+                    <Icon>{iconName}</Icon>
+                </Col>
+                <Col s={9} align="center">
+                    <Col m={4} s={4} xs={6}>
+                        <TextInput
+                            type="number"
+                            min="0"
+                            max="23"
+                            value={hourInput}
+                            onChange={handleChangeHourInput}
+                        />
+                    </Col>
+                    <Col className="teste">:</Col>
+                    <Col m={4} s={4} xs={6}>
+                        <TextInput
+                            type="number"
+                            min="0"
+                            max="59"
+                            value={minuteInput}
+                            onChange={handleChangeMinuteInput}
+                        />
+                    </Col>
+                </Col>
+            </Row>
+            {/* <Row>
+                <Col s={3} align="right">
+                    Hours
+                </Col>
+                <Col s={8}>
+                    <Range
+                        max="23"
+                        min="0"
+                        name="points1"
+                        step="1"
+                        value={startHourRange}
+                        onChange={handleChangeStartHourRange}
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col s={3} align="right">
+                    Minutes
+                </Col>
+                <Col s={8}>
+                    <Range
+                        max="59"
+                        min="0"
+                        name="points2"
+                        step="1"
+                        value={startMinuteRange}
+                        onChange={handleChangeStartMinuteRange}
+                    />
+                </Col>
+            </Row> */}
+        </Card>
+    );
+};
+
+export default CustomTimePicker;
--- src/Pages/HoursWorkedFlowchart/HoursWorkedFlowchart.js
+++ src/Pages/HoursWorkedFlowchart/HoursWorkedFlowchart.js
@@ -1,73 +1,100 @@
-import React, {useRef} from 'react';
-import {Icon, Row, Col, TimePicker} from 'react-materialize';
+import React, {useState} from 'react';
+import {
+    Icon,
+    Row,
+    Col,
+    TimePicker,
+    Range,
+    TextInput,
+    Button,
+    Card,
+} from 'react-materialize';
 
+import CustomTimePicker from 'Components/CustomTimePicker';
 import moment from 'moment';
 
 import {HOME_START_INPUT, HOME_FINISH_INPUT} from '../../Constants/Texts';
 import classes from './HoursWorkedFlowchart.module.css';
 
-const timePickerOptions = {
-    autoClose: false,
-    container: null,
-    defaultTime: 'now',
-    duration: 0,
-    fromNow: 0,
-    i18n: {
-        cancel: 'Cancel',
-        clear: 'Clear',
-        done: 'Ok',
-    },
-    onCloseEnd: null,
-    onCloseStart: null,
-    onOpenEnd: null,
-    onOpenStart: null,
-    onSelect: null,
-    showClearBtn: false,
-    twelveHour: false,
-    vibrate: true,
+const newHour = (hour) => moment(hour).format('HH');
+const newMinute = (minute) => moment(minute).format('mm');
+const formattedMoment = (hour, minute) => {
+    if (hour && minute) {
+        return moment().set({hour, minute}).format('HH:mm');
+    }
+    return 'ERROR';
 };
 
 const HoursWorkedFlowchart = (props) => {
-    const startTimePikerRef = useRef('');
-    const finishTimePikerRef = useRef('');
+    const [startHourInput, setStartHourInput] = useState('');
+    const [startMinuteInput, setStartMinuteInput] = useState('');
+    const [finishHourInput, setFinishHourInput] = useState('');
+    const [finishMinuteInput, setFinishMinuteInput] = useState('');
 
-    const handleChangeStartTimePicker = (hour, minute) => {
-        startTimePikerRef.current = moment(`${hour}:${minute}`);
+    const handleChangeStartHourInput = (e) => {
+        setStartHourInput(e.target.value);
+    };
+    const handleChangeStartMinuteInput = (e) => {
+        setStartMinuteInput(e.target.value);
+    };
+    const handleChangeFinishHourInput = (e) => {
+        setFinishHourInput(e.target.value);
+    };
+    const handleChangeFinishMinuteInput = (e) => {
+        setFinishMinuteInput(e.target.value);
     };
 
-    const handleChangeFinishTimePicker = (hour, minute) => {
-        finishTimePikerRef.current = moment(`${hour}:${minute}`);
-    };
+    // const [startHourRange, setStartHourRange] = useState(0);
+    // const [startMinuteRange, setStartMinuteRange] = useState(0);
+    // const handleChangeStartHourRange = (e) => {
+    //     console.log(e);
+    //     setStartHourRange(e.target.value);
+    // };
+    // const handleChangeStartMinuteRange = (e) => {
+    //     console.log(e);
+    //     setStartMinuteRange(e.target.value);
+    // };
+    // console.log('RANGE', `${startHourRange}:${startMinuteRange}`);
+
+    console.log('START INPUT', `${startHourInput}:${startMinuteInput}`);
+    console.log(
+        'startFormatted',
+        formattedMoment(startHourInput, startMinuteInput)
+    );
+
+    console.log('FINISH INPUT', `${finishHourInput}:${finishMinuteInput}`);
+    console.log(
+        'finishFormatted',
+        formattedMoment(finishHourInput, finishMinuteInput)
+    );
 
     return (
         <>
-            <Row>
-                <Col s={4}>
-                    {HOME_START_INPUT}
-                    <Row>
-                        <TimePicker
-                            ref={startTimePikerRef}
-                            icon={<Icon>schedule</Icon>}
-                            value={startTimePikerRef.current}
-                            onChange={handleChangeStartTimePicker}
-                            options={timePickerOptions}
-                        />
-                    </Row>
-                    {HOME_FINISH_INPUT}
-                    <Row>
-                        <TimePicker
-                            ref={finishTimePikerRef}
-                            icon={<Icon>schedule</Icon>}
-                            value={finishTimePikerRef.current}
-                            onChange={handleChangeFinishTimePicker}
-                            options={timePickerOptions}
-                        />
+            <Row s={12}>
+                <Col l={4} m={6} s={12}>
+                    <CustomTimePicker
+                        title={HOME_START_INPUT}
+                        iconName="schedule"
+                        hourInput={startHourInput}
+                        handleChangeHourInput={handleChangeStartHourInput}
+                        minuteInput={startMinuteInput}
+                        handleChangeMinuteInput={handleChangeStartMinuteInput}
+                    />
+                    <CustomTimePicker
+                        title={HOME_FINISH_INPUT}
+                        iconName="watch_later"
+                        hourInput={finishHourInput}
+                        handleChangeHourInput={handleChangeFinishHourInput}
+                        minuteInput={finishMinuteInput}
+                        handleChangeMinuteInput={handleChangeFinishMinuteInput}
+                    />
+                    <Row align="right">
+                        <Button>Generate Chart</Button>
                     </Row>
                 </Col>
-                <Col s={8}>Chart</Col>
+                <Col>Chart</Col>
             </Row>
         </>
     );
 };
-
 export default HoursWorkedFlowchart;
